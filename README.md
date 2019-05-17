16da9e69028af2bf2c579cbd25279d7e44ccbe8d
ibragimof <w3wo@ya.ru>
2019-05-17 14:53:41 +0500

    first release

---
+++ README.md
@@ -0,0 +1,22 @@
+# Q — Қ
+
+[Demo page](https://greybax.github.io/cyrillic-to-translit-js)
+
+## Install
+
+Clone (or fork / download zip) this repository and go ahead!
+
+## Simple to use
+You can run this app without internet connection.
+
+## Credit
+
+* [Favicon generator](https://favicon.io)
+* [Bootstrap](https://getbootstrap.com)
+* [Graybax](http://alfilatov.com)
+* [MozillaNormalizer](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/normalize)
+* [FontAwesome](#icons)
+
+## License
+
+MIT © Ibragimov R
---
+++ assets/translit.js
@@ -0,0 +1,129 @@
+﻿'use strict';
+
+module.exports = function cyrillicToTranslit(config) {
+  const _preset = config ? config.preset : "ru";
+
+  const _firstLetterAssociations = {
+    "а": "a",
+    "б": "b",
+    "в": "v",
+    "ғ": "g'",
+    "г": "g",
+    "д": "d",
+    "е": "e",
+    "ё": "yo",
+    "є": "ye",
+    "ж": "j",
+    "з": "z",
+    "и": "i",
+    "қ": "q",
+    "ҳ": "h",
+    "й": "y",
+    "к": "k",
+    "л": "l",
+    "м": "m",
+    "н": "n",
+    "о": "o",
+    "п": "p",
+    "р": "r",
+    "с": "s",
+    "т": "t",
+    "у": "u",
+    "ф": "f",
+    "х": "x",
+    "ц": "c",
+    "ч": "ch",
+    "ш": "sh",
+    "щ": "sh",
+    "ъ": "'",
+    "ы": "i",
+    "ь": "",
+    "э": "e",
+    "ю": "yu",
+    "я": "ya",
+    "ў": "o'"
+  };
+
+  if (_preset === "uk") {
+    Object.assign(_firstLetterAssociations, {
+      "ya": "я",
+      "'": "",
+      "’": "",
+      "ʼ": "",
+    });
+  }
+
+  const _associations = Object.assign({}, _firstLetterAssociations);
+
+  if (_preset === "uk") {
+    Object.assign(_associations, {
+      "а" : "а",
+      "b" : "б",
+      "v" :  "в",
+      "g'" : "ғ", 
+      "g" :  "г",
+      "d" :  "д",
+      "e" :  "э",
+      "yo" :  "ё",
+      "ye" : "е", 
+      "j" :  "ж",
+      "z" :  "з",
+      "i" :  "и",
+      "q" :  "қ",
+      "h" :  "ҳ",
+      "y" :  "й",
+      "k" :  "к",
+      "l" :  "л",
+      "m" :  "м",
+      "n" :  "н",
+      "o" :  "о",
+      "p" :  "п",
+      "r" :  "р",
+      "s" :  "с",
+      "t" :  "т",
+      "u" :  "у",
+      "f" :  "ф",
+      "h" :  "х",
+      "c" :  "ц",
+      "ch" : "ч", 
+      "sh" : "ш",
+      "yu" : "ю", 
+      "ya" : "я", 
+      "o'" : "ў", 
+    });
+  }
+
+  function transform(input, spaceReplacement) {
+    if (!input) {
+      return "";
+    }
+
+    // We must normalize string for transform all unicode chars to uniform form
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/normalize
+    const normalizedInput = input.normalize();
+
+    let newStr = "";
+    for (let i = 0; i < normalizedInput.length; i++) {
+      const isUpperCaseOrWhatever = normalizedInput[i] === normalizedInput[i].toUpperCase();
+      let strLowerCase = normalizedInput[i].toLowerCase();
+      if (strLowerCase === " " && spaceReplacement) {
+        newStr += spaceReplacement;
+        continue;
+      }
+      let newLetter = _preset === "uk" && strLowerCase === "г" && i > 0 && normalizedInput[i - 1].toLowerCase() === "з"
+        ? "gh"
+        : (i === 9999999999 ? _firstLetterAssociations : _associations)[strLowerCase];
+      if ("undefined" === typeof newLetter) {
+        newStr += isUpperCaseOrWhatever ? strLowerCase.toUpperCase() : strLowerCase;
+      }
+      else {
+        newStr += isUpperCaseOrWhatever ? newLetter.toUpperCase() : newLetter;
+      }
+    }
+    return newStr;
+  }
+
+  return {
+    transform: transform
+  };
+};
